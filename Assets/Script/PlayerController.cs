e46248792d4786260dd7b7060180325706c6b5da
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2023-10-29 03:47:57 +0900

    [fix]グラップリングの完成

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -10,10 +10,14 @@ public class PlayerController : MonoBehaviour
     Vector3 _dir;
     Camera _mainCamera;
     /// <summary>グラップリングのスピード </summary>
-    [SerializeField] float _grapSpeed = 3.0f;
+    float _grapSpeed;
+    /// <summary>グラップリングの届く距離/// </summary>
     [SerializeField] float _grapDis = 50.0f;
     [SerializeField] float _jumpPower = 3.0f;
     [SerializeField] Transform _debugHitPointTransform;
+    [SerializeField] Transform _grapShotTransform;
+    /// <summary>グラップの長さ </summary>
+    float _grapSize;
     Vector3 vec;
     RaycastHit hit;
     /// <summary>法線のベクトル </summary>
@@ -25,12 +29,23 @@ public class PlayerController : MonoBehaviour
     Vector3 _grapShotPos;
     /// <summary>グラップリングの終点位置からの距離 </summary>
     [SerializeField] float _reachGrapPos = 1.0f;
+    /// <summary>グラップの速度の最大値/// </summary>
+    [SerializeField] float _grapSpeedMax = 40f;
+    /// <summary>グラップの速度の最小値/// </summary>
+    [SerializeField] float _grapSpeedMin = 5f;
+    bool _jumping = false;
     State _state;
+    [SerializeField] ParticleSystem _particleSystem;
+    //接地判定に必要な変数
+    [SerializeField] float _groundDistance = 0.2f;
+    [SerializeField] LayerMask _groundMask = ~0;
+    CapsuleCollider _capsuleCollider;
+    [SerializeField] GameObject _camera;
     enum State
     {
         Normal,
-        FookShot,
-
+        GrapShotFlying,
+        GrapTrow
     }
     // Start is called before the first frame update
     void Start()
@@ -40,14 +55,21 @@ public class PlayerController : MonoBehaviour
         //カーソルを出さないようにする
         Cursor.lockState = CursorLockMode.Locked;
         _state = State.Normal;
+        _capsuleCollider = GetComponent<CapsuleCollider>();
+        _grapShotTransform.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_state == State.FookShot)
+        //IsGround = CheckGround();
+        if (_state == State.GrapShotFlying)
         {
             GrapringMove();
+        }
+        else if(_state == State.GrapTrow)
+        {
+            GrapShotThrow();
         }
         else if (_state == State.Normal)
         {
@@ -75,56 +97,41 @@ public class PlayerController : MonoBehaviour
             //    //this.transform.forward = forward;
 
             //}
-
-            if (Input.GetButtonDown("Jump"))
-            {
-                Debug.Log("ジャンプ");
-            }
-
             if (Input.GetMouseButtonDown(0))
             {
                 GrapringStart();
             }
 
-        }
-        //Debug.DrawRay(this.transform.position, _mainCamera.transform.forward * _grapDis, Color.green, 2.0f, false);
-    }
+            if(IsGround)
+            {
+                if (Input.GetButtonDown("Jump"))
+                {
+                    _jumping = true;
+                }
+            }
+            else
+            {
+                _jumping = false;
+            }
 
-    void GrapringStart()
-    {
-        RaycastHit hitGrap;
-        if (Physics.Raycast(this.transform.position, _mainCamera.transform.forward, out hitGrap, _grapDis))
-        {
-            _debugHitPointTransform.transform.position = hitGrap.point;
-            _grapShotPos = hitGrap.point;
-            _state = State.FookShot;
-            //this.transform.DOMove(hitGrap.transform.position, _grapSpeed);
-            //Debug.Log(hitGrap.collider.gameObject.name);
         }
-    }
-
-    void GrapringMove()
-    {
-        //グラップリングの方向を決める
-        Vector3 grapDir = (_grapShotPos - transform.position).normalized;
-        //移動させる
-        _rb.velocity =  grapDir * _grapSpeed;
-        //終点についた時の処理
-        if(Vector3.Distance(transform.position, _grapShotPos) < _reachGrapPos)
-        {
-            _state = State.Normal;
-        }
-        Debug.Log("フックショット中");
+        Debug.Log(IsGround);
     }
 
     void FixedUpdate()
     {
-        if(_state == State.Normal)
+        if (_state == State.Normal)
         {
             //プレイヤーの移動処理
             if (IsGround)
             {
                 vec = Vector3.ProjectOnPlane(vec, _normalVector);
+                if (_jumping)
+                {
+                    vec.y = _jumpPower;
+                    IsGround = false;
+                    Debug.Log("ジャンプ");
+                }
                 _rb.velocity = vec;
             }
             else
@@ -134,12 +141,67 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    //接地判定
-    bool CheckGround()
+    void GrapringStart()
     {
-        //return Physics.SphereCast()
-        return true;
+        RaycastHit hitGrap;
+        if (Physics.Raycast(this.transform.position, _mainCamera.transform.forward, out hitGrap, _grapDis))
+        {
+            _debugHitPointTransform.transform.position = hitGrap.point;
+            _grapShotPos = hitGrap.point;
+            _grapSize = 0f;
+            _grapShotTransform.gameObject.SetActive(true);
+            _grapShotTransform.localScale = Vector3.zero;
+            _state = State.GrapTrow;
+        }
     }
+
+    private void GrapShotThrow()
+    {
+        _grapShotTransform.LookAt(_grapShotPos);
+        float grapThrowSpeed = 500f;
+        _grapSize += grapThrowSpeed * Time.deltaTime;
+        _grapShotTransform.localScale = new Vector3(1, 1, _grapSize);
+
+        if(_grapSize >= Vector3.Distance(transform.position, _grapShotPos))
+        {
+            _state = State.GrapShotFlying;
+            _particleSystem.Play();
+        }
+    }
+    void GrapringMove()
+    {
+        IsGround = false;
+        _grapShotTransform.LookAt(_grapShotPos);
+        //グラップリングの方向を決める
+        Vector3 grapDir = (_grapShotPos - transform.position).normalized;
+        //遠くなればなるほど早くして、近づくほど原則する
+        _grapSpeed = Mathf.Clamp(Vector3.Distance(transform.position, _grapShotPos), _grapSpeedMin, _grapSpeedMax);
+        float _grapSpeedMultiplier = 2f;
+        //移動させる
+        _rb.velocity =  grapDir * _grapSpeed * _grapSpeedMultiplier;
+        //終点についた時の処理
+        if(Vector3.Distance(transform.position, _grapShotPos) < _reachGrapPos)
+        {
+            StopGrapShot();
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            //グラップリングキャンセル
+            StopGrapShot();
+        }
+
+        //if (Input.GetButtonDown("Jump"))
+        //{
+
+        //}
+    }
+
+    //接地判定
+    //bool CheckGround()
+    //{
+        
+    //}
 
     private void OnCollisionStay(Collision collision)
     {
@@ -148,14 +210,21 @@ public class PlayerController : MonoBehaviour
         _normalVector = collision.contacts[0].normal;
     }
 
-    private void OnTriggerStay(Collider other)
+    private void ResetGravity()
+    {
+        vec.y = 0;
+    }
+
+    private void OnCollisionEnter(Collision collision)
     {
         IsGround = true;
     }
-    private void OnTriggerExit(Collider other)
-    {
-        IsGround = false;
-    }
 
-    
+    private void StopGrapShot()
+    {
+        _state = State.Normal;
+        ResetGravity() ;
+        _grapShotTransform.gameObject.SetActive(false);
+        _particleSystem.Stop();
+    }
 }
